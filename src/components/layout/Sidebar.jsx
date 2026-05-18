c3451000360d200e80686156b835a66e260dcaf4
Jecinaha <hagg.jennifer@gmail.com>
2026-05-18 08:58:58 +0200

    Restrict signup to admin users

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext.jsx";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const { user } = useAuth();
+
   return (
     <aside className="sidebar">
       <nav className="sidebar-nav">
         <NavLink to="/" end className="sidebar-link">
-         <i class="fa-solid fa-house-user"></i>
+          <i className="fa-solid fa-house-user"></i>
           <span>Dashboard</span>
         </NavLink>
 
@@ -29,6 +32,13 @@ function Sidebar() {
           <i className="fa-solid fa-user"></i>
           <span>Profil</span>
         </NavLink>
+
+        {user?.role === "Admin" && (
+          <NavLink to="/signup" className="sidebar-link">
+            <i className="fa-solid fa-user-plus"></i>
+            <span>Skapa användare</span>
+          </NavLink>
+        )}
       </nav>
     </aside>
   );
